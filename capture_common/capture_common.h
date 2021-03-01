4ac21f44fa73aa87213002148167d76aadde3eb8
lawrence <liuxiaoli@zhongguang.com>
2021-03-01 14:11:57 +0800

    feat: add json format

---
+++ capture_common/capture_common.h
@@ -0,0 +1,15 @@
+#ifndef _CAPTRUE_COMMON_H
+#define _CAPTRUE_COMMON_H
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+int json_read(void);
+int setJson(cJSON *root);
+
+#ifdef __cplusplus
+}
+#endif
+#endif  //_COMMON_INTERFACE_H
